45aecb8b058ec13afe8daa18e9972a68b6e3dc7a
jshoughtaling <jared.houghtaling@tuftsmedicalcenter.org>
2024-05-09 12:39:54 -0400

    Clean up and add docs

--- convert.R
+++ convert.R
@@ -1,29 +1,12 @@
 library(rmarkdown)
 
-setwd("/mnt/c/Users/JHoughtalingadmin/Documents/GitHub/chorus-developer")
-#list.files("/mnt/c/Users/JHoughtalingadmin/Documents/GitHub/chorus-developer/Rmd")
+setwd("<path to chorus-developer repo locally>")
 
 fileNames<-Sys.glob("./Rmd/*.Rmd")
 #print(fileNames)
 
 rmarkdown::render_site(
-  input = "/mnt/c/Users/JHoughtalingadmin/Documents/GitHub/chorus-developer/Rmd",
+  input = "<path to chorus-developer repo locally>/Rmd",
   output_format = "all"
 )
 
-
-#for (fileName in fileNames) {
-#  fileOnly <- strsplit(fileName, split='/')[[1]][3]
-#  nameOnly <- strsplit(fileOnly, split='\\.')[[1]][1]
-#  # read data:
-#  rmarkdown::render(
-#    input = fileName,
-#    output_format = "all",
-#    output_file = paste0("/mnt/c/Users/JHoughtalingadmin/Documents/GitHub/chorus-developer/test/", nameOnly, ".html"),
-#    encoding = "UTF-8",
-#    clean = TRUE,
-#    run_pandoc = TRUE
-#  )  
-#  # add more stuff here
-  
-#}
